0223d3b0e962d4e562ebe83cd4a0d8b0b0f77ed8
Balaphani <phnkumar51@gmail.com>
2024-01-31 22:44:48 +0000

    Backend setup2

--- backend.sh
+++ backend.sh
@@ -3,17 +3,16 @@ dnf module enable nodejs:18 -y
 
 dnf install nodejs -y
 
+cp backend.service /etc/systemd/system/backend.service
+
 useradd expense
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
-pwd
 cd /app
 
-pwd
 unzip /tmp/backend.zip
 npm install
 
-cp backend.service /etc/systemd/system/backend.service
 
 systemctl daemon-reload
 systemctl enable backend
