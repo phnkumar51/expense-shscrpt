84d87be436cd9747e3d283c006097bf3d05f64ac
Balaphani <phnkumar51@gmail.com>
2024-02-04 16:30:21 +0000

    Print the description of the backend service with colors

--- backend.sh
+++ backend.sh
@@ -1,31 +1,31 @@
-echo -e"\e[33m disable previous version of nodejs\e[0m"
+echo -e "\e[31m disable previous version of nodejs\e[0m"
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 
-echo -e"\e[33m install the latest version of nodejs\e[0m"
+echo -e "\e[31m install the latest version of nodejs\e[0m"
 dnf install nodejs -y
 
-echo -e"\e[33m configure the backend file\e[0m"
+echo -e "\e[31m configure the backend file\e[0m"
 cp backend.service /etc/systemd/system/backend.service
 
-echo -e"\e[33m add user\e[0m"
+echo -e "\e[31m add user\e[0m"
 useradd expense
 
-echo -e"\e[33m remove directory\e[0m"
+echo -e "\e[31m remove directory\e[0m"
 rm -rf /app
 
-echo -e"\e[33m add directory\e[0m"
+echo -e "\e[31m add directory\e[0m"
 mkdir /app
 
-echo -e"\e[33m download the content\e[0m"
+echo -e "\e[31m download the content\e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 
-echo -e"\e[33m unzip the content\e[0m"
+echo -e "\e[31m unzip the content\e[0m"
 unzip /tmp/backend.zip
 npm install
 
-echo -e"\e[33m restart the webserver\e[0m"
+echo -e "\e[31m restart the webserver\e[0m"
 systemctl daemon-reload
 systemctl enable backend
 systemctl restart backend
