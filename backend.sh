bbecccda0f2d576175a9bb97c123a07d0dbb9e12
Balaphani <phnkumar51@gmail.com>
2024-02-04 16:00:03 +0000

    Print the description of the backend service

--- backend.sh
+++ backend.sh
@@ -1,20 +1,31 @@
+echo disable previous version of nodejs
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 
+echo install the latest version of nodejs
 dnf install nodejs -y
 
+echo configure the backend file
 cp backend.service /etc/systemd/system/backend.service
 
+echo add user
 useradd expense
+
+echo remove directory
 rm -rf /app
+
+echo add directory
 mkdir /app
+
+echo download the content
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 
+echo unzip the content
 unzip /tmp/backend.zip
 npm install
 
-
+echo restart the webserver
 systemctl daemon-reload
 systemctl enable backend
 systemctl restart backend
