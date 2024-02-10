a319317549ae9c3dd50d7ace4eb455ecbc9c734f
Balaphani <phnkumar51@gmail.com>
2024-02-10 12:20:45 +0000

    Variable declaration

--- backend.sh
+++ backend.sh
@@ -1,34 +1,36 @@
+log_file=$log_file
+
 echo -e "\e[31m disable previous version of nodejs\e[0m"
-dnf module disable nodejs -y &>/tmp/expense.log
-dnf module enable nodejs:18 -y &>/tmp/expense.log
+dnf module disable nodejs -y &>$log_file
+dnf module enable nodejs:18 -y &>$log_file
 
 echo -e "\e[31m install the latest version of nodejs\e[0m"
-dnf install nodejs -y &>/tmp/expense.log
+dnf install nodejs -y &>$log_file
 
 echo -e "\e[31m configure the backend file\e[0m"
-cp backend.service /etc/systemd/system/backend.service &>/tmp/expense.log
+cp backend.service /etc/systemd/system/backend.service &>$log_file
 
 echo -e "\e[31m add user\e[0m"
-useradd expense &>/tmp/expense.log
+useradd expense &>$log_file
 
 echo -e "\e[31m remove directory\e[0m"
-rm -rf /app &>/tmp/expense.log
+rm -rf /app &>$log_file
 
 echo -e "\e[31m add directory\e[0m"
-mkdir /app &>/tmp/expense.log
+mkdir /app &>$log_file
 
 echo -e "\e[31m download the content\e[0m"
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>/tmp/expense.log
-cd /app &>/tmp/expense.log
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>$log_file
+cd /app &>$log_file
 
 echo -e "\e[31m unzip the content\e[0m"
-unzip /tmp/backend.zip &>/tmp/expense.log
-npm install &>/tmp/expense.log
+unzip /tmp/backend.zip &>$log_file
+npm install &>$log_file
 
 echo -e "\e[31m restart the webserver\e[0m"
-systemctl daemon-reload &>/tmp/expense.log
-systemctl enable backend &>/tmp/expense.log
-systemctl restart backend &>/tmp/expense.log
+systemctl daemon-reload &>$log_file
+systemctl enable backend &>$log_file
+systemctl restart backend &>$log_file
 
-dnf install mysql -y &>/tmp/expense.log
-mysql -h mysql-dev.bdevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>/tmp/expense.log
+dnf install mysql -y &>$log_file
+mysql -h mysql-dev.bdevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>$log_file
